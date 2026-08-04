b90b774f668d3647de7c8ec2ffd762b454c5baa7
chacafdz <chacafdz@gmail.com>
2026-08-04 15:21:09 -0600

    Funcion de Guardar y
    se creo la funcion de guardar los marcadores

---
+++ BreakOut/Assets/_Scriptable Objects/PuntajeAlto.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "PuntajeAlto", menuName = "Tools/PuntajeAlto", order =0)]
+public class PuntajeAlto : PuntajePersistente
+
+{
+    public int puntaje = 0;
+    public int puntajeAlto = 10000;
+
+}
